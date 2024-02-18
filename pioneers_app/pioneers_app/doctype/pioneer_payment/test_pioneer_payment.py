046424c9f3fe67be133046b6f2452fe69a487e22
ali <straoid@gmail.com>
2024-02-18 03:21:30 +0200

    create application files

---
+++ pioneers_app/pioneers_app/doctype/pioneer_payment/test_pioneer_payment.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, ali.frg.c@gmail.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPioneerPayment(FrappeTestCase):
+	pass
