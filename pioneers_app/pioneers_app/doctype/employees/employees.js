3d5715dc2ce4b74f21e35e5360ab263e7d1c51d6
ali <straoid@gmail.com>
2024-04-21 04:04:26 +0200

    add employee doctype

---
+++ pioneers_app/pioneers_app/doctype/employees/employees.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, ali.frg.c@gmail.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Employees", {
+// 	refresh(frm) {
+
+// 	},
+// });
